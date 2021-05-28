4bf9d9254b2d1fa6706a466c5a99f6d92fe474ae
murgupluoglu <masadov@gmail.com>
2021-05-28 15:56:23 +0300

    EnvironmentActivity and SdkStartActivity added

---
+++ app/src/main/java/com/inventiv/gastropaysdk/sample/EnvironmentActivity.kt
@@ -0,0 +1,58 @@
+package com.inventiv.gastropaysdk.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+import com.google.gson.Gson
+import com.inventiv.gastropaysdk.shared.Environment
+
+class EnvironmentActivity : AppCompatActivity() {
+
+    private val buttonDev: MaterialButton by lazy {
+        findViewById(R.id.devEnvButton)
+    }
+    private val buttonPilot: MaterialButton by lazy {
+        findViewById(R.id.pilotEnvButton)
+    }
+    private val buttonTest: MaterialButton by lazy {
+        findViewById(R.id.testEnvButton)
+    }
+    private val buttonProduction: MaterialButton by lazy {
+        findViewById(R.id.productionEnvButton)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_environment)
+
+        buttonDev.setOnClickListener {
+            saveAndGo(Environment.DEV)
+        }
+        buttonPilot.setOnClickListener {
+            saveAndGo(Environment.PILOT)
+        }
+        buttonTest.setOnClickListener {
+            saveAndGo(Environment.TEST)
+        }
+        buttonProduction.setOnClickListener {
+            saveAndGo(Environment.PRODUCTION)
+        }
+
+        val sharedPref = getSharedPref()
+        val strInfos = sharedPref.getString(PREF_INFOS, String())
+        if (!strInfos.isNullOrEmpty()) {
+            val info = Gson().fromJson(strInfos, InfoModel::class.java)
+            saveAndGo(info.environment)
+        }
+    }
+
+    private fun saveAndGo(environment: Environment) {
+        val infoModel = InfoModel(environment)
+        val info = Gson().toJson(infoModel)
+        getSharedPref().edit().putString(PREF_INFOS, info).apply()
+
+
+        startActivity(SdkStartActivity.newIntent(this, infoModel))
+        finish()
+    }
+}
---
+++ app/src/main/java/com/inventiv/gastropaysdk/sample/InfoModel.kt
@@ -0,0 +1,10 @@
+package com.inventiv.gastropaysdk.sample
+
+import android.os.Parcelable
+import com.inventiv.gastropaysdk.shared.Environment
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class InfoModel(
+    val environment: Environment,
+) : Parcelable
