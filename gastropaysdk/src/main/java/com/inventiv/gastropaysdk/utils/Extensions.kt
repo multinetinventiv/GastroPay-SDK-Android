549557d8a93a6a9837c28feede0305204d901502
murgupluoglu <masadov@gmail.com>
2021-06-23 11:45:28 +0300

    Extensions.kt updated

--- gastropaysdk/src/main/java/com/inventiv/gastropaysdk/utils/Extensions.kt
+++ gastropaysdk/src/main/java/com/inventiv/gastropaysdk/utils/Extensions.kt
@@ -1,6 +1,10 @@
 package com.inventiv.gastropaysdk.utils
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
 import com.inventiv.gastropaysdk.R
 
 internal fun Context.getDistanceAsMeters(distance: Int): String {
@@ -14,4 +18,24 @@ internal fun Context.getDistanceAsMeters(distance: Int): String {
     } else {
         resources.getString(R.string.merchant_distance_label_mt_gastropay_sdk, distance.toString())
     }
+}
+
+/**
+ * This shows a UI with the number being dialed, allowing the user to explicitly initiate the call.
+ * @return false if exception exist
+ */
+fun Context.openPhoneDialer(phoneNumber: String): Boolean = try {
+    val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null))
+    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+    startActivity(intent)
+    true
+} catch (e: ActivityNotFoundException) {
+    Log.e("openPhoneDialer", e.toString())
+    false
+}
+
+fun Context.openGoogleMap(latitude: Double, longitude: Double) {
+    val uri = Uri.parse("http://maps.google.com/maps?daddr=${latitude},${longitude}")
+    val intent = Intent(Intent.ACTION_VIEW, uri)
+    startActivity(intent)
 }
