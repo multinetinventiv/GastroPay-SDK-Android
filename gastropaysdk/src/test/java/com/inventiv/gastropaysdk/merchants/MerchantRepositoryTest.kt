0e2351f0590b4e8015981b490fb07280f252a777
murgupluoglu <masadov@gmail.com>
2021-06-24 10:56:06 +0300

    MerchantRepositoryTest.kt updated, MerchantDetailViewModelTest.kt added

--- gastropaysdk/src/test/java/com/inventiv/gastropaysdk/merchants/MerchantRepositoryTest.kt
+++ gastropaysdk/src/test/java/com/inventiv/gastropaysdk/merchants/MerchantRepositoryTest.kt
@@ -3,6 +3,7 @@ package com.inventiv.gastropaysdk.merchants
 import com.google.common.truth.Truth
 import com.inventiv.gastropaysdk.data.model.Resource
 import com.inventiv.gastropaysdk.data.model.response.Merchant
+import com.inventiv.gastropaysdk.data.model.response.MerchantDetail
 import com.inventiv.gastropaysdk.data.model.response.MerchantPaging
 import com.inventiv.gastropaysdk.repository.MerchantRepository
 import com.inventiv.gastropaysdk.utils.loadingFalseExpected
@@ -19,7 +20,7 @@ class MerchantRepositoryTest {
     private val repository: MerchantRepository = mock()
 
     @Test
-    fun `merchant repository success test`() = runBlocking {
+    fun `getMerchants repository success test`() = runBlocking {
         // Given
         val expected = MerchantPaging(
             merchants = listOf(Merchant("", "", 0.0, 0.0, 0, null, null, null, null, null)),
@@ -38,6 +39,55 @@ class MerchantRepositoryTest {
 
         // Then
         repository.getMerchants(0.0, 0.0, null, null, 1).collectIndexed { index, response ->
+            if (index == 0) {
+                response.loadingTrueExpected()
+            }
+            if (index == 1) {
+                Truth.assertThat(response is Resource.Success)
+                val success = response as Resource.Success
+                Truth.assertThat(success.data).isEqualTo(expected)
+            }
+            if (index == 2) {
+                response.loadingFalseExpected()
+            }
+        }
+
+    }
+
+    @Test
+    fun `getMerchantDetail repository success test`() = runBlocking {
+        // Given
+        val expected = MerchantDetail(
+            merchantId = "1",
+            tags = arrayListOf(),
+            pageContent = null,
+            address = null,
+            name = "Test",
+            logoUrl = null,
+            images = arrayListOf(),
+            isBonusPoint = false,
+            rewardPercentage = null,
+            distance = null,
+            latitude = 0.0,
+            longitude = 0.0,
+            note = null,
+            rate = 0f,
+            showcaseImageUrl = null,
+            phoneNumber = null,
+            gsmNumber = null
+        )
+
+        val flow = flow {
+            emit(Resource.Loading(true))
+            emit(Resource.Success(expected))
+            emit(Resource.Loading(false))
+        }
+
+        // When
+        whenever(repository.getMerchantDetail("1")).thenReturn(flow)
+
+        // Then
+        repository.getMerchantDetail("1").collectIndexed { index, response ->
             if (index == 0) {
                 response.loadingTrueExpected()
             }
